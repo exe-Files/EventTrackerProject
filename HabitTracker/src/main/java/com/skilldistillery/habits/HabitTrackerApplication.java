043f868ce027e6764dc64f826809c30e8467c226
Brandon <files.brandon@gmail.com>
2021-08-02 10:47:53 -0500

    add project dependencies to build.grade, application.properties, Application.java

--- HabitTracker/src/main/java/com/skilldistillery/habits/HabitTrackerApplication.java
+++ HabitTracker/src/main/java/com/skilldistillery/habits/HabitTrackerApplication.java
@@ -2,10 +2,15 @@ package com.skilldistillery.habits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HabitTrackerApplication {
-
+public class HabitTrackerApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(HabitTrackerApplication.class);
+	  }
 	public static void main(String[] args) {
 		SpringApplication.run(HabitTrackerApplication.class, args);
 	}
